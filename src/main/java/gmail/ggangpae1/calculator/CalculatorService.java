0ddbccffebb43722d066aaaacde73043f8079f3c
Jongho Lee <hopaper2367@naver.com>
2025-07-18 11:36:01 +0900

    jenkins test

---
+++ src/main/java/gmail/ggangpae1/calculator/CalculatorService.java
@@ -0,0 +1,10 @@
+package gmail.ggangpae1.calculator;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class CalculatorService {
+    int sum(Integer a, Integer b){
+        return a+b;
+    }
+}
