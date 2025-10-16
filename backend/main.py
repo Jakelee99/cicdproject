25f02f1f89ebed14f7068bff4b46e50cd6ea1026
Jongho Lee <hopaper2367@naver.com>
2025-10-16 19:05:36 +0900

    new EC2에 적용

--- backend/main.py
+++ backend/main.py
@@ -19,7 +19,7 @@ app = FastAPI(title="QR Code Question API")
 origins = [
     "http://localhost:5173",  #React 개발 서버
     "http://127.0.0.1:5173",
-    "http://43.202.252.37:5173"
+    "http://3.38.110.249:5173"
 ]
 
 app.add_middleware(
