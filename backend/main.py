35a96c9e6b4450a212a691ad22af467993957022
Jongho Lee <hopaper2367@naver.com>
2025-10-20 01:36:24 +0900

    new_commit

--- backend/main.py
+++ backend/main.py
@@ -19,7 +19,7 @@ app = FastAPI(title="QR Code Question API")
 origins = [
     "http://localhost:5173",  #React 개발 서버
     "http://127.0.0.1:5173",
-    "http://3.38.110.249:5173"
+    "http://3.39.242.66:5173"
 ]
 
 app.add_middleware(
